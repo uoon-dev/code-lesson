0351a4282c736e0583c00b94f0a17912b37e66c3
최장호 <hmchoi651@naver.com>
2019-11-16 14:28:18 +0900

    star step 1

--- step02/star01.js
+++ step02/star01.js
@@ -6,3 +6,13 @@
 ****
 *****
 */
+let string = "";
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j <= i; j++) {
+    debugger;
+    string += "*";
+  }
+  string += "\n";
+  debugger;
+}
+console.log(string);
