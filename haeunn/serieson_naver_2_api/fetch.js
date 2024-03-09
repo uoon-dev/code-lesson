1397e3d632e41479b0c171563dd2a28b6e5a6913
2HaEunn <zzonga328@naver.com>
2024-03-09 09:06:02 +0900

    '영화인목록'

---
+++ haeunn/serieson_naver_2_api/fetch.js
@@ -0,0 +1,136 @@
+function _fetch(url, options = {}) {
+  const promise = new Promise((resolve, reject) => {
+    // 1. HTTP 통신 요청을 할 수 있게 도와주는 객체를 만든다.
+    const xhr = new XMLHttpRequest();
+
+    // 2. URL 주소로 method 방식에 맞게 통신을 하겠다. (준비)
+    xhr.open(options.method || "GET", url);
+
+    // 3. 요청을 수행하고, 응답을 받으면 어떻게 할 것인지를 정의한다.
+    xhr.onload = () => {
+      // 서버에 요청을 보내고 성공했을 때 받는 응답 코드
+      if (xhr.status >= 200 && xhr.status < 300) {
+        console.log("xhr", xhr);
+
+        resolve({
+          status: xhr.status,
+          statusText: xhr.statusText,
+          json: () => Promise.resolve(JSON.parse(xhr.responseText)),
+        });
+      } else {
+        reject({
+          status: xhr.status,
+          statusText: xhr.statusText + "실패!!",
+        });
+      }
+    };
+
+    // 네트워크 에러 처리
+    xhr.onerror = function () {
+      reject({
+        status: xhr.status,
+        statusText: xhr.statusText,
+      });
+    };
+
+    // 4. 요청을 보낸다.
+    xhr.send(options.body || null);
+  });
+
+  return promise;
+}
+
+const url =
+  "http://kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?key=f5eef3421c602c6cb7ea224104795888&test=1";
+
+_fetch(url)
+  .then((res) => {
+    //   console.log(res.status + ' : ' + res.statusText);
+
+    return res.json();
+  })
+  .then((data) => {
+    const peopleList = data.peopleListResult.peopleList;
+    const ul = document.querySelector(".list");
+    const movieName = document.querySelector(".movieNm");
+    const moviePerson = document.querySelector(".person");
+    const movieRole = document.querySelector(".role");
+
+    ul.innerHTML = peopleList
+      .map(
+        (people) => `<li>
+        <img src="/images/지금 이대로가 좋아.png"/>
+        <p>
+        <div class="sales-movie-name">
+        ${people.filmoNames}
+        </div>
+        <div class="sales-movie-info">
+        ${people.peopleNm}(${people.repRoleNm})
+        </div>
+        </p>
+        </li>`
+      )
+      .join("");
+  });
+
+// const searchInput = document.querySelector("#search");
+// const ul = document.querySelector(".list");
+
+// window.addEventListener("keydown", (e) => {
+//   if (e.key === "Enter") {
+//     const movieNm = searchInput.value;
+//     _fetch(
+//       `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=f5eef3421c602c6cb7ea224104795888&movieNm=${movieNm}`
+//     )
+//       .then((res) => res.json())
+//       .then((data) => {
+//         const movieList = data.movieListResult.movieList;
+//         ul.innerHTML = movieList
+//           .map((movie) => {
+//             return `<li>
+//                   <img class="movie-img" src="/images/지금 이대로가 좋아.png" />
+//                   <p>
+//                   <div class="sales-movie-name">
+//                   ${movie.movieNm}
+//                   </div>
+//                   <div class="sales-movie-info">
+//                   ${movie.typeNm}:(${movie.openDt})
+//                   </div>
+//                   </p>
+//                 </li>`;
+//           })
+//           .join("");
+//       });
+//   }
+// });
+
+const searchInput = document.querySelector("#search");
+const ul = document.querySelector(".list");
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const peopleNm = searchInput.value;
+    _fetch(
+      `http://kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?key=f5eef3421c602c6cb7ea224104795888&peopleNm=${peopleNm}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const peopleList = data.peopleListResult.peopleList;
+        ul.innerHTML = peopleList
+          .map((people) => {
+            return `<li>
+                  <img class="movie-img" src="/images/지금 이대로가 좋아.png" />
+                  <p>
+                  <div class="sales-movie-name">
+                  ${people.peopleNm}(${people.repRoleNm})
+                  </div>
+                  <div class="sales-movie-info">
+                  ${people.filmoNames}
+                  </div>
+                  </p>
+                </li>`;
+          })
+          .join("");
+      });
+  }
+});
